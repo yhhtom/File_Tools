0e52adca8b825fff166fa2c9d78c1ee8cc32a3ab
PD20W <yuanhaohang@outlook.com>
2025-12-05 13:03:50 +0800

    修了bug

---
+++ README.md
@@ -0,0 +1,13 @@
+通过python来提升重命名大量文件的速度，和批量修改文件的扩展名。
+
+Speed up renaming a large number of files by python, and modify the extension of multiple files at the same time.
+
+一个学习过程中写的小项目，灵感来源于打codeforces的时候创建ABCDE.cpp很麻烦，以及像把.jupyter丢给AI的时候重命名成.txt很麻烦。之后可能会加其他功能。
+
+A small project written during learning, inspired by the need to create ABCDE.cpp when playing codeforces, and the need to rename .jupyter to .txt when giving it to AI, may add other features later.
+
+直接点击run.bat运行，然后根据显示内容操作就行。
+
+Just run run.bat, and follow the instructions.
+
+🙂😇😍🤖☺️😋
--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from colorama import init,Fore,Style
 init()
 print(Fore.GREEN+'PD20W File Tools ver. 0.0.1'+Style.RESET_ALL)
 folder_path=input(Fore.BLUE+'Enter nothing to use the default Input folder, or enter your folder path: '+Style.RESET_ALL)
+current_folder=os.path.dirname(os.path.abspath(__file__))
+if(folder_path==''):
+    folder_path=os.path.join(current_folder,'Input')
 while not os.path.exists(folder_path):
     folder_path=input('Path not found! Please try again')
 while os.path.isfile(folder_path):
     folder_path=input('This is not a folder! Please try again')
-current_folder=os.path.dirname(os.path.abspath(__file__))
-if(folder_path==''):
-    folder_path=os.path.join(current_folder,'Input')
+print('Enter "quit" to quit')
 def main():
     operation=input(Fore.BLUE+'Welcome! Enter the tool name to use it! Enter "tools" to see all the tools: '+Style.RESET_ALL)
+    if operation=='quit':
+        return
     try:
         tool_module = getattr(My_File_Tools, operation)
         main_func = getattr(tool_module, 'main')
@@ -20,8 +23,11 @@ def main():
     except AttributeError:
         print(f"Error: Tool '{operation}' not found in My_File_Tools, please try again!")
         main()
+        return
     except Exception as e:
         print(f"Error running tool: {e}, please try again!")
         main()
+        return
+    main()
 main()
 print(Fore.GREEN+'See you again!'+Style.RESET_ALL)
